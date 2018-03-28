86205d240f6c9cbcff502efdf5821aeafdb47fe5
James Foley <jamesrichmondfoley@googlemail.com>
2018-03-28 11:39:59 +0100

    Initial commit

---
+++ testing/routing.py
@@ -0,0 +1,13 @@
+from testing.apps.site import routing
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
